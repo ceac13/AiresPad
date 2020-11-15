e452d3cee53fa4983fd769e79f6f8983b3d5eabf
Carlos Aires <ceac13@gmail.com>
2020-11-15 18:33:12 -0300

    Change parameters

--- AiresPad.ino
+++ AiresPad.ino
@@ -22,14 +22,14 @@ char pinAssignments[13] ={'A0','A1','A2','A3','A4','A5','A6','A7','A8','A9', 'A1
 byte padNote[13] =       { 4,   49 , 53 , 51 , 48 , 43 ,  1 , 37 , 38 , 18 ,  36  ,   5  ,    6 }; // MIDI notes from 0 to 127 (Mid C = 60)
 bool padActive[13] =     {true, true, true, true, true, true, true, true, true, true, true, true, true};
 bool hihat[13] =         {true, false, false, false, false, false, false, false, false, false, false, false, false};
-int threshold[13] =      {10, padS, padS, padS, padS, padS, padS, padS, padS, padS, 60, 20, 20}; // Minimum value to get trigger
-float gain[13] =      {1.0, padG, padG, padG, padG, padG, padG, padG, padG, padG, 2.0, 2.0, 2.0}; // multiplier to apply in the analog pin values
+int threshold[13] =      {10, padS, padS, padS, padS, padS, padS, padS, 50, padS, 60, 20, 20}; // Minimum value to get trigger
+float gain[13] =      {1.0, padG, padG, padG, padG, padG, padG, padG, 2.7, padG, 2.0, 2.0, 2.0}; // multiplier to apply in the analog pin values
 int maskTime[13] =      {15, 4, 4, 4, 4, 4, 4, 4, 4, 4, 40, 4, 4}; // Minimum number of cycles to a new trigger. It should to be bigger than the others attributes.
 int scanTime =          3; // Time hearing the pad to decide the correct value
 float retrigger =       0.6; // New trigger only value is greater than <<retrigger>> * last value
 //int maskTime =          30; // Minimum number of cycles to a new trigger. It should to be bigger than the others attributes.
 long crossTalk =         8; // Number of milliseconds where cannot have more than one trigger. Highest first
-double crosstalkRatio = 2.0; // Less than crosstalkRatio * threshold will be removed 
+double crosstalkRatio = 2.2; // Less than crosstalkRatio * threshold will be removed 
 //float gain =            1.0; // multiplier to apply in the analog pin values
 
 int numberOfPads = 13;
@@ -332,7 +332,7 @@ void sendMidi(byte MESSAGE, byte PITCH, byte VELOCITY) {
 }
 
 int calculateVelocity(int value, int pin) {
-  int minimo = 90;
+  int minimo = 70;
   double newValue = value - threshold[pin];
   double dthreshold = threshold[pin];
   double taxa = 127 / (1023 - dthreshold);
